e4c8f074007a813d05f7b620f51284b5ea9207ed
amberylx <amberyl.x@gmail.com>
2016-10-22 19:57:29 -0700

    Add work on push notification

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.template.response import TemplateResponse
@@ -6,9 +9,25 @@ from django.template.response import TemplateResponse
 def home(request):
     return TemplateResponse(request, 'home.html')
 
-
 def admin(request):
     return TemplateResponse(request, 'admin.html')
 
+def send_push_notification(request):
+    headers = {
+        'Authorization': 'key=AIzaSyAxJyzPAclQsMyoNos6DJT4Zh-gLEty_Go',
+        'Content-Type': 'application/json',
+    }
+    data = {
+        "to" : "",
+        "notification" : {
+            "title": "Portugal vs. Denmark",
+            "body": "5 to 1"
+        },
+    }
+    response = requests.post('https://fcm.googleapis.com/fcm/send',
+                             headers=headers, data=json.dumps(data))
+    print response.content
+    return HttpResponse('')
+
 def user(request):
-    return TemplateResponse(request, 'user.html')
+    return TemplateResponse(request, 'cover-user.html')
